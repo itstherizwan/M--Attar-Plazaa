44272e482679a8f039e84634ff1f739aa4c66f58
itstherizwan <136790304+itstherizwan@users.noreply.github.com>
2023-07-01 23:55:45 +0530

    Update Email Content in  userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,6 +32,59 @@ export const register = async (
       "Verify Your Account - One-Time Password (OTP)",
       `<html>
         <!-- Email content -->
+        <head>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              background-color: #D6DBDF;
+              margin: 0;
+              padding: 20px;
+            }
+            .container {
+              max-width: 600px;
+              margin: 0 auto;
+              background-color: #F1F3C7;
+              padding: 20px;
+              border-radius: 5px;
+              box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            }
+            h1 {
+              color: 030B40;
+              font-size: 24px;
+              margin-bottom: 20px;
+            }
+            p {
+              color: #2F0136;
+              font-size: 16px;
+              line-height: 1.5;
+              margin-bottom: 10px;
+            }
+            .otp {
+              background-color: #F92803;
+              padding: 10px;
+              text-align:center;
+              font-size: 20px;
+              font-weight: bold;
+              margin-bottom: 20px;
+            }
+            .contact {
+              color: #888888;
+              font-size: 14px;
+              margin-top: 30px;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>Verify Your Account - One-Time Password (OTP)</h1>
+            <p>Dear ${name},</p>
+            <p>Thank you for registering with our service. To complete your account verification, please use the following One-Time Password (OTP):</p>
+            <div class="otp">${otp}</div>
+            <p>Please enter this OTP on the verification page within 5 minutes to verify your account.</p>
+            <p>If you did not request this OTP or have any concerns regarding your account, please contact our support team immediately at <a href="mailto:m.attar.plazaa@gmail.com">m.attar.plazaa@gmail.com</a>.</p>
+            <p class="contact">Best regards,<br>M-Attar Plazaa</p>
+          </div>
+        </body>
       </html>`
     );
 
