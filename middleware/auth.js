e91ce9808fb85ca8a8125f9853fd37d2228dbb2c
itstherizwan <136790304+itstherizwan@users.noreply.github.com>
2023-07-02 22:22:15 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,23 +3,7 @@
 
 
 
-// export const isAuthenticated = async (req, res, next) => {
-//   try {
-//     const { token } = req.cookies;
 
-//     if (!token) {
-//       return res.status(401).json({ success: false, message: "Login First" });
-//     }
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//     req.user = await User.findById(decoded._id);
-
-//     next();
-//   } catch (error) {
-//     res.status(500).json({ success: false, message: error.message });
-//   }
-// };
 import jwt from "jsonwebtoken";
 import { User } from "../models/userModel.js";
 import ErrorHandler from "../utils/Errorhandler.js";
@@ -34,15 +18,7 @@ export const isAuthenticated = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    if (!decoded || !decoded._id) {
-      return res.status(401).json({ success: false, message: "Invalid Token" });
-    }
-
     req.user = await User.findById(decoded._id);
-
-    if (!req.user) {
-      return res.status(401).json({ success: false, message: "User not found" });
-    }
 
     next();
   } catch (error) {
